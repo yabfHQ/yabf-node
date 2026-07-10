f38f4fbbcad5fa78ad05f47ff1764bdd628c18c8
Gits <briangits@proton.me>
2026-07-10 14:24:48 +0300

    feat(framer/binary): add framer config
    - add & implement maxFrameSize

--- framing/src/binary/BinaryFramer.ts
+++ framing/src/binary/BinaryFramer.ts
@@ -1,16 +1,22 @@
 import { FrameTagPosition } from '../FrameTagPosition'
 import { Framer } from '../Framer'
 import { IncompleteStreamError, InvalidFrameTagError } from '../errors'
+import { FrameSizeExceededError } from '../errors/FrameSizeExceededError'
 import { BinaryFrame } from './BinaryFrame'
+import { BinaryFramerConfig } from './BinaryFramerConfig'
 import { growableBuffer } from './GrowableBuffer'
 
 export interface BinaryFramer extends Framer {}
 
-export function binaryFramer(): BinaryFramer {
+/* TODO: Default formatting looks ugly, add some sort of rule to fix this */
+export function binaryFramer({
+    maxFrameSize = 16 * 1024 * 1024
+}: Partial<BinaryFramerConfig> = {}): BinaryFramer {
     return {
         async *encode(input) {
             for await (const chunk of input) {
                 const payloadSize = chunk.length
+                if (payloadSize > maxFrameSize) frameSizeError(payloadSize, maxFrameSize)
 
                 const frame = new Uint8Array(BinaryFrame.frameSize(payloadSize))
                 const view = new DataView(frame.buffer)
@@ -49,6 +55,8 @@ export function binaryFramer(): BinaryFramer {
                     if (buf.available < BinaryFrame.Header.SIZE) break
 
                     const payloadSize = buf.view().getUint32(BinaryFrame.Header.LENGTH_OFFSET)
+                    if (payloadSize > maxFrameSize) frameSizeError(payloadSize, maxFrameSize)
+
                     const frameSize = BinaryFrame.frameSize(payloadSize)
                     if (buf.available < frameSize) break
 
@@ -56,10 +64,7 @@ export function binaryFramer(): BinaryFramer {
                     const endTag = buf.byteAt(tailOffset)
                     if (endTag !== BinaryFrame.Tags.END) tagError(endTag, FrameTagPosition.END)
 
-                    const payload = buf.slice(
-                        BinaryFrame.PAYLOAD_OFFSET,
-                        tailOffset
-                    )
+                    const payload = buf.slice(BinaryFrame.PAYLOAD_OFFSET, tailOffset)
 
                     buf.consume(frameSize)
                     yield payload
@@ -80,4 +85,12 @@ function tagError(tag: number, position: FrameTagPosition): never {
         position === FrameTagPosition.START ? BinaryFrame.Tags.START : BinaryFrame.Tags.END
 
     throw new InvalidFrameTagError(stringify(expected), stringify(tag), position)
+}
+
+function frameSizeError(frameSize: number, maxFrameSize: number): never {
+    function stringify(size: number) {
+        return `${size} bytes`
+    }
+
+    throw new FrameSizeExceededError(stringify(maxFrameSize), stringify(frameSize))
 }
---
+++ framing/src/binary/BinaryFramerConfig.ts
@@ -0,0 +1,3 @@
+export interface BinaryFramerConfig {
+    maxFrameSize: number
+}
