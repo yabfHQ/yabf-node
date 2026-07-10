b2b04a5532fd9b8d119fe4c3a0c3cb427580be8b
Gits <briangits@proton.me>
2026-07-10 14:57:22 +0300

    fix(framer): add binary framer to exports

--- framing/src/binary/index.ts
+++ framing/src/binary/index.ts
@@ -1,1 +1,3 @@
+export * from './BinaryFrame'
 export * from './BinaryFramer'
+export * from './BinaryFramerConfig'
--- framing/src/index.ts
+++ framing/src/index.ts
@@ -2,4 +2,6 @@ export * from './Framer'
 export * from './FrameTag'
 export * from './FrameTagPosition'
 
+export { BinaryFrame, binaryFramer, type BinaryFramer, type BinaryFramerConfig } from './binary'
+
 export * from './errors'
